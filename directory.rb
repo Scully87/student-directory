9a1edcba0700d5f64303cf226f556aeef749f64f
Scully87 <scully87.ts@googlemail.com>
2014-08-29 10:59:55 -0700

    Added Comments to Code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The potential names of my fellow students ha"
 puts "--------------------------------------"
 puts "Toby Lerone"
@@ -9,6 +10,8 @@ puts "Chris P Bird"
 puts "Liz Anier"
 puts "Karl Hickbread"
 puts "Mark N Spencer"
+#finally, we print the list of students
 print "Overall, we have "
+#it's important that print () doesn't add new line characters
 print 9
 puts " crazy students"
